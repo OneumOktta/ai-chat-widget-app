4366ca175b5736a3ff0c21327220287b9b398163
Evgeny Abrosimov <evga.abrosimov@gmail.com>
2024-12-19 14:40:20 +0300

    fix reg

--- app/pages/registration.vue
+++ app/pages/registration.vue
@@ -44,110 +44,100 @@
 </script>
 
 <template>
-  <div>
-    <div class="mx-auto w-1/3 min-w-[400px]">
-      <div class="flex-1">
-        <h2 class="mb-10 text-center text-3xl font-semibold">
-          Создайте аккаунт
-        </h2>
+  <div class="mx-auto w-1/3 min-w-[400px]">
+    <h2 class="mb-10 text-center text-3xl font-semibold">Создайте аккаунт</h2>
 
-        <form class="space-y-4" novalidate @submit.prevent>
-          <BaseInput
-            id="name"
-            v-model="formData.name"
-            label="Ваше имя"
-            icon="carbon:user-avatar"
-            required
-          />
-          <BaseInput
-            id="email"
-            v-model="formData.email"
-            type="email"
-            label="Электронная почта"
-            icon="basil:envelope-outline"
-          />
-          <BaseInput
-            id="password"
-            v-model="formData.password"
-            type="password"
-            label="Пароль"
-            icon="basil:lock-outline"
-            required
-          />
-          <BaseInput
-            id="confirmPassword"
-            v-model="formData.passwordConfirm"
-            type="password"
-            label="Подтвердите пароль"
-            icon="basil:lock-outline"
-            required
-          />
+    <form class="space-y-4" novalidate @submit.prevent>
+      <BaseInput
+        id="name"
+        v-model="formData.name"
+        label="Ваше имя"
+        icon="carbon:user-avatar"
+        required
+      />
+      <BaseInput
+        id="email"
+        v-model="formData.email"
+        type="email"
+        label="Электронная почта"
+        icon="basil:envelope-outline"
+      />
+      <BaseInput
+        id="password"
+        v-model="formData.password"
+        type="password"
+        label="Пароль"
+        icon="basil:lock-outline"
+        required
+      />
+      <BaseInput
+        id="confirmPassword"
+        v-model="formData.passwordConfirm"
+        type="password"
+        label="Подтвердите пароль"
+        icon="basil:lock-outline"
+        required
+      />
 
-          <div class="mt-8 space-y-4 pt-[24px]">
-            <BaseCheckbox v-model="acceptTerms">
-              Я согласен с
-              <NuxtLink
-                to="/terms"
-                class="text-lightPink transition-colors hover:text-lightBlue"
+      <div class="mt-8 space-y-4 pt-[24px]">
+        <BaseCheckbox v-model="acceptTerms">
+          Я согласен с
+          <NuxtLink
+            to="/terms"
+            class="text-lightPink transition-colors hover:text-lightBlue"
+          >
+            Правилами и условиями
+          </NuxtLink>
+          Oneum и
+          <NuxtLink
+            to="/privacy"
+            class="text-lightPink transition-colors hover:text-lightBlue"
+          >
+            Политикой конфиденциальности
+          </NuxtLink>
+        </BaseCheckbox>
+
+        <div class="space-y-10">
+          <div v-if="auth.error" class="text-center text-red-500">
+            {{ auth.error }}
+          </div>
+
+          <GradientButton type="button" class="w-full" @click="handleRegister">
+            Зарегистрироваться
+          </GradientButton>
+
+          <div class="text-center">
+            <p class="mb-4 text-light-text/60 dark:text-dark-text/60">
+              Вы можете зарегистрироваться с помощью:
+            </p>
+            <div class="flex justify-center gap-4 text-dark-text">
+              <button
+                class="flex items-center gap-2 rounded-full bg-lightBlue px-4 py-4 transition-colors"
               >
-                Правилами и условиями
-              </NuxtLink>
-              Oneum и
-              <NuxtLink
-                to="/privacy"
-                class="text-lightPink transition-colors hover:text-lightBlue"
+                <Icon name="basil:vk-solid" class="h-6 w-6" />
+              </button>
+              <button
+                class="flex items-center gap-2 rounded-full bg-lightBlue px-4 py-4 transition-colors"
               >
-                Политикой конфиденциальности
-              </NuxtLink>
-            </BaseCheckbox>
-
-            <div class="space-y-10">
-              <div v-if="auth.error" class="text-center text-red-500">
-                {{ auth.error }}
-              </div>
-
-              <GradientButton
-                type="button"
-                class="w-full"
-                @click="handleRegister"
-              >
-                Зарегистрироваться
-              </GradientButton>
-
-              <div class="text-center">
-                <p class="mb-4 text-light-text/60 dark:text-dark-text/60">
-                  Вы можете зарегистрироваться с помощью:
-                </p>
-                <div class="flex justify-center gap-4 text-dark-text">
-                  <button
-                    class="flex items-center gap-2 rounded-full bg-lightBlue px-4 py-4 transition-colors"
-                  >
-                    <Icon name="basil:vk-solid" class="h-6 w-6" />
-                  </button>
-                  <button
-                    class="flex items-center gap-2 rounded-full bg-lightBlue px-4 py-4 transition-colors"
-                  >
-                    <Icon name="basil:telegram-solid" class="h-6 w-6" />
-                  </button>
-                </div>
-              </div>
-
-              <div class="text-center">
-                <p class="text-light-text/60 dark:text-dark-text/60">
-                  У вас уже есть аккаунт?
-                  <NuxtLink
-                    to="/login"
-                    class="text-lightPink transition-colors hover:text-lightBlue"
-                  >
-                    Вход
-                  </NuxtLink>
-                </p>
-              </div>
+                <Icon name="basil:telegram-solid" class="h-6 w-6" />
+              </button>
             </div>
           </div>
-        </form>
+
+          <div class="text-center">
+            <p class="text-light-text/60 dark:text-dark-text/60">
+              У вас уже есть аккаунт?
+              <NuxtLink
+                to="/login"
+                class="text-lightPink transition-colors hover:text-lightBlue"
+              >
+                Вход
+              </NuxtLink>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
+    </form>
 
     <!-- Модальное окно успешной регистрации -->
     <div
