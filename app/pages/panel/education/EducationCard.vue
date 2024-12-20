ffcf6959cadf278f87598269d3046ef7123205c6
Roman Ulitin <kurt-77@mail.ru>
2024-12-20 15:10:30 +0500

    added education page

---
+++ app/pages/panel/education/EducationCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="relative h-[200px] cursor-pointer rounded-2xl border border-purple-300 bg-light-panels p-[25px] shadow-sm transition hover:scale-105 hover:border-[#3F00A2] hover:shadow-md dark:bg-dark-panels"
+  >
+    <div>
+      <div class="flex items-center justify-between">
+        <h2 class="mb-2 text-lg font-bold">Нейросеть</h2>
+        <div class="flex items-center gap-2 text-xs text-gray-400">
+          <span class="flex items-center gap-1">
+            <span class="flex flex-col items-center gap-1">
+              <img src="/time.svg" alt="" />
+              30 мин
+            </span>
+          </span>
+          <span class="flex items-center gap-1">
+            <span class="flex flex-col items-center gap-1">
+              <img src="/level.svg" alt="" />
+              Средний
+            </span>
+          </span>
+        </div>
+      </div>
+      <p class="text-sm text-gray-500">
+        Как обучить нейросеть продавать товары без менеджера
+      </p>
+    </div>
+    <NuxtLink to="/panel/neuronet">
+      <div
+        class="mt-4 w-full rounded-2xl bg-gradient-to-r from-purple-500 to-pink-500 p-3 text-center text-white shadow-lg transition duration-200 hover:scale-105"
+      >
+        Начать
+      </div>
+    </NuxtLink>
+  </div>
+</template>
---
+++ app/pages/panel/education/index.vue
@@ -0,0 +1,52 @@
+<script setup>
+  import DropdownFilter from '~/pages/panel/education/DropdownFilter.vue'
+  import EducationCard from '~/pages/panel/education/EducationCard.vue'
+  definePageMeta({
+    layout: 'panel',
+  })
+  // Опции для каждого списка
+  const difficultyOptions = ['Легкий', 'Средний', 'Сложный']
+  const timeOptions = ['30 минут', 'Час', 'Два часа']
+  const progressOptions = ['Начато', 'В процессе', 'Завершенно']
+</script>
+
+<template>
+  <PanelContent>
+    <!-- Заголовок -->
+    <div class="mb-6 flex items-center justify-between">
+      <div class="flex items-center gap-10">
+        <img src="/left.svg" alt="" />
+        <h1 class="text-3xl font-bold">Обучение</h1>
+      </div>
+      <div
+        class="mr-[86px] w-1/4 rounded-2xl border border-purple-300 px-7 py-2 hover:border-[#3F00A2]"
+      >
+        <p class="mb-1 text-sm text-gray-500">Ваш прогресс</p>
+        <div class="relative h-2 w-full rounded-full bg-gray-200">
+          <div
+            class="h-2 rounded-full bg-gradient-to-r from-purple-500 to-pink-500"
+            style="width: 20%"
+          ></div>
+        </div>
+        <span class="text-xs text-gray-600">20% Новичок</span>
+      </div>
+    </div>
+
+    <!-- Фильтры -->
+    <div class="flex items-start justify-between gap-4 p-6 px-[86px]">
+      <!-- Список 1 -->
+      <DropdownFilter title="Уровень сложности" :options="difficultyOptions" />
+
+      <!-- Список 2 -->
+      <DropdownFilter title="Время изучения" :options="timeOptions" />
+
+      <!-- Список 3 -->
+      <DropdownFilter title="Прогресс" :options="progressOptions" />
+    </div>
+
+    <!-- Grid карточек -->
+    <div class="grid grid-cols-1 gap-6 px-[86px] md:grid-cols-2 lg:grid-cols-3">
+      <EducationCard v-for="n in 6" :key="n" />
+    </div>
+  </PanelContent>
+</template>
